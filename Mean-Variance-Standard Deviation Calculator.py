08b555b9ff0fd269f9387609707c9bb2dfb88012
LyNhutMinh <139345066+LyNhutMinh@users.noreply.github.com>
2026-04-19 20:39:36 +0700

    Update Mean-Variance-Standard Deviation Calculator.py

--- Mean-Variance-Standard Deviation Calculator.py
+++ Mean-Variance-Standard Deviation Calculator.py
@@ -44,3 +44,9 @@ def calculate(list):
     }
 
     return calculations
+
+
+if __name__ == '__main__':
+    sample_data = [0,1,2,3,4,5,6,7,8]
+    result = calculate(sample_data)
+    print(result)
